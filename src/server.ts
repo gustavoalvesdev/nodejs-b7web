316983a75aca3ef061751dd5b0a2ee4bda60ad6e
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-12-09 00:40:41 -0300

    Criação de rotas no Express 1

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,32 @@ server.use(express.urlencoded({extended: true}));
 // middleware para configurar a pasta ./public da aplicação
 server.use(express.static(path.join(__dirname, '../public')));
 
-// requisição get
+// ROTAS
+
+server.get('/ping', (req, res) => {
+    res.json({pong: true});
+});
+
+server.get('/produtos', (req, res) => {
+    res.json({produtos: [
+        {
+            id: 2,
+            name: 'Farinha Dona Benta',
+            price: 13.34,
+            qty: 12,
+            min_qty: 5
+        },
+        {
+            id: 7,
+            name: 'Macarrão Renata Spaguetti',
+            price: 3.54,
+            qty: 20,
+            min_qty: 9
+        }
+
+    ]})
+});
+
 server.get('/', (req, res) => {
 
     let name: string = 'Gustavo';

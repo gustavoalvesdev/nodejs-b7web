1d19be9a17224f787a37368a461de951af12d927
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-12-09 01:01:34 -0300

    Criação de rotas no Express 2

--- src/server.ts
+++ src/server.ts
@@ -40,6 +40,39 @@ server.get('/produtos', (req, res) => {
     ]})
 });
 
+server.get('/produtos/:id', (req, res) => {
+    
+    let produtos = [
+        {
+            id: 2,
+            name: 'Farinha Dona Benta',
+            price: 13.34,
+            qty: 12,
+            min_qty: 5
+        },
+        {
+            id: 7,
+            name: 'Macarrão Renata Spaguetti',
+            price: 3.54,
+            qty: 20,
+            min_qty: 9
+        }
+
+    ];
+
+    let id: number = parseInt(req.params.id);
+    let produto = {};
+    for (let i = 0; i < produtos.length; i++) {
+        if (produtos[i].id == id) {
+
+            produto = produtos[i];
+
+        }
+    }
+    
+    res.json(produto)
+});
+
 server.get('/', (req, res) => {
 
     let name: string = 'Gustavo';
